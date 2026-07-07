d69cb4a32bc8f9e31aa113158339ab0935be7f35
itszubariel <anishchapagain00@gmail.com>
2026-07-08 01:08:31 +0900

    fix suppress unwanted errors in console

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body className={inter.className}>
+      <body className={inter.className} suppressHydrationWarning>
         <Background />
         <Navbar />
         {children}
