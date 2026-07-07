e0336ba4ac19cec0359393332c07619acaf70c1b
itszubariel <anishchapagain00@gmail.com>
2026-07-08 01:09:53 +0900

    update add on demand buffer to suppress unwanted errors

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,10 @@ const withNextra = nextra({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  onDemandEntries: {
+    maxInactiveAge: 25 * 1000,
+    pagesBufferLength: 2,
+  },
 };
 
 export default withNextra(nextConfig);
