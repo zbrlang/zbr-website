de1fd80bab6d7f2e7a81391351325383a3942a34
itszubariel <anishchapagain00@gmail.com>
2026-08-08 22:57:38 +1000

    add normalizer back

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import "nextra-theme-docs/style.css";
 import type { AppProps } from "next/app";
 import "../app/globals.css";
+import DocsLinkNormalizer from "../components/DocsLinkNormalizer";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <DocsLinkNormalizer />
+      <Component {...pageProps} />
+    </>
+  );
 }
